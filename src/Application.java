b00957a8dd403d02d0182e070d84b91a6d2792c6
Gabriel Araujo <gabriel-araujo1020@hotmail.com>
2021-04-04 15:49:27 -0300

    Criação variavel nome

--- src/Application.java
+++ src/Application.java
@@ -4,6 +4,8 @@ public class Application {
 	public static void main(String[] args) {
 		// Apenas um teste
 		System.out.println("Bom dia");
+		String nome = "Gabriel";
+		System.out.println(nome);
 	}
 
 }
